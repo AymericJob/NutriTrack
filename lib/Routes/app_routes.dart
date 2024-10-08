85f8de49dc60ac55209237ecb45393db5a053aea
aymericjob <aymeric.job@student.hepl.be>
2024-10-08 18:26:59 +0200

    :zap:

--- lib/Routes/app_routes.dart
+++ lib/Routes/app_routes.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import '../Pages/Logs/login_page.dart';
 import '../Pages/Logs/register_page.dart';
-import '../Pages/Home/main_page.dart'; // Importer la page principale
+import '../Pages/Home/main_page.dart';
 
 class AppRoutes {
   static const String loginPage = '/login';
   static const String registerPage = '/register';
   static const String mainPage = '/main';
 
-  // Méthode pour générer les routes
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case loginPage:
@@ -16,13 +15,9 @@ class AppRoutes {
       case registerPage:
         return MaterialPageRoute(builder: (_) => RegisterPage());
       case mainPage:
-        return MaterialPageRoute(builder: (_) => MainPage()); // Remplacez par la page que vous avez créée
+        return MaterialPageRoute(builder: (_) => MainPage());
       default:
-        return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(child: Text('404 - Page Not Found')),
-          ),
-        );
+        return MaterialPageRoute(builder: (_) => LoginPage());
     }
   }
 }
