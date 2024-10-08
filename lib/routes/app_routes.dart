18670af1d2c08279789ad0b54487f0ffa874bd5c
aymericjob <aymeric.job@student.hepl.be>
2024-10-08 17:49:33 +0200

    :zap:

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import '../pages/home_page.dart';
 import '../pages/login_page.dart';
 import '../pages/register_page.dart';
+import '../pages/main_page.dart'; // Importer la page principale
 
 class AppRoutes {
   static const String home = '/';
   static const String login = '/login';
   static const String register = '/register';
+  static const String mainPage = '/main';  // Route pour la page principale
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -16,6 +18,8 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => LoginPage());
       case register:
         return MaterialPageRoute(builder: (_) => RegisterPage());
+      case mainPage:
+        return MaterialPageRoute(builder: (_) => MainPage());  // Page principale
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
