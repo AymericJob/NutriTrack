de6d05418cf880e4d97d2597c795b683d676623b
aymericjob <aymeric.job@student.hepl.be>
2024-10-19 12:07:27 +0200

    :zap: Update routes

--- lib/Routes/app_routes.dart
+++ lib/Routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../Pages/Logs/login_page.dart';
 import '../Pages/Logs/register_page.dart';
+import '../Pages/Logs/home_page.dart';  // Ajout de HomePage
 import '../Pages/Home/main_page.dart';
 import '../Pages/Home/Profile/profile_page.dart';
 import '../Pages/Home/Profile/personal_info_page.dart';
@@ -9,7 +10,7 @@ import '../Pages/Home/Profile/activity_tracking_page.dart';
 
 class AppRoutes {
   // Anciennes routes
-  static const String homePage = '/';
+  static const String homePage = '/homepage';
   static const String loginPage = '/login';
   static const String registerPage = '/register';
   static const String mainPage = '/main';
@@ -20,9 +21,12 @@ class AppRoutes {
   static const String nutritionGoalsPage = '/nutrition_goals';
   static const String activityTrackingPage = '/activity_tracking';
 
+  // Gestionnaire de routes
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
     // Anciennes routes
+      case homePage:  // Redirige vers HomePage
+        return MaterialPageRoute(builder: (_) => HomePage());
       case loginPage:
         return MaterialPageRoute(builder: (_) => LoginPage());
       case registerPage:
@@ -30,7 +34,7 @@ class AppRoutes {
       case mainPage:
         return MaterialPageRoute(builder: (_) => MainPage());
 
-    // Nouvelles routes
+    // Nouvelles routes pour le profil
       case profilePage:
         return MaterialPageRoute(builder: (_) => ProfilePage());
       case personalInfoPage:
@@ -40,9 +44,21 @@ class AppRoutes {
       case activityTrackingPage:
         return MaterialPageRoute(builder: (_) => ActivityTrackingPage());
 
-    // Route par défaut
+    // Route par défaut (en cas de route non trouvée)
       default:
-        return MaterialPageRoute(builder: (_) => LoginPage());
+        return _errorRoute();
     }
+  }
+
+  // Fonction pour afficher une page d'erreur
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Error')),
+        body: Center(
+          child: Text('Page not found'),
+        ),
+      );
+    });
   }
 }
