5293b5d53033412eb02b59f8ba5887abff82b048
aymeric <aymeric.job@student.hepl.be>
2024-12-10 23:47:50 +0100

    :construction: change on the search interface, all on the same page. Design also needs to be redone

--- lib/Pages/Models/add_food_page.dart
+++ lib/Pages/Models/add_food_page.dart
@@ -2,11 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
-import '../models/food.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/food.dart';
 import '../Models/FoodDetailPage.dart';
-import 'FoodSearchPage.dart';
 
 class AddFoodPage extends StatefulWidget {
   final Function(Food) onFoodAdded;
@@ -24,6 +23,10 @@ class _AddFoodPageState extends State<AddFoodPage> {
   final _carbsController = TextEditingController();
   final _fatController = TextEditingController();
   final _proteinController = TextEditingController();
+  final _searchController = TextEditingController();
+
+  List<Food> _foodList = [];
+  bool _isLoading = false;
 
   void _resetFields() {
     _nameController.clear();
@@ -61,21 +64,6 @@ class _AddFoodPageState extends State<AddFoodPage> {
           _carbsController.text = product['nutriments']?['carbohydrates']?.toString() ?? '0';
           _fatController.text = product['nutriments']?['fat']?.toString() ?? '0';
           _proteinController.text = product['nutriments']?['proteins']?.toString() ?? '0';
-
-          final food = Food(
-            name: _nameController.text,
-            calories: int.tryParse(_caloriesController.text) ?? 0,
-            carbs: int.tryParse(_carbsController.text) ?? 0,
-            fat: int.tryParse(_fatController.text) ?? 0,
-            protein: int.tryParse(_proteinController.text) ?? 0,
-          );
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => FoodDetailPage(food: food),
-            ),
-          );
         } else {
           _showMessage("Produit non trouvé.");
         }
@@ -84,6 +72,79 @@ class _AddFoodPageState extends State<AddFoodPage> {
       }
     } catch (e) {
       _showMessage("Erreur de connexion.");
+    }
+  }
+
+  Future<void> _searchFoods() async {
+    final query = _searchController.text;
+    if (query.isEmpty) return;
+
+    setState(() {
+      _isLoading = true;
+      _foodList.clear();
+    });
+
+    try {
+      final openFoodFactsResults = _fetchFromOpenFoodFacts(query);
+      final edamamResults = _fetchFromEdamamAPI(query);
+      final results = await Future.wait([openFoodFactsResults, edamamResults]);
+
+      setState(() {
+        _foodList = results.expand((r) => r).toList();
+      });
+    } catch (e) {
+      _showMessage("Erreur de recherche : $e");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<List<Food>> _fetchFromOpenFoodFacts(String query) async {
+    final url = 'https://world.openfoodfacts.org/cgi/search.pl?search_terms=$query&json=true';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final products = data['products'] as List;
+      return products.map((product) {
+        return Food(
+          name: product['product_name'] ?? 'Inconnu',
+          calories: (product['nutriments']?['energy-kcal'] ?? 0).toInt(),
+          carbs: (product['nutriments']?['carbohydrates'] ?? 0).toInt(),
+          fat: (product['nutriments']?['fat'] ?? 0).toInt(),
+          protein: (product['nutriments']?['proteins'] ?? 0).toInt(),
+          sourceApi: "OpenFoodFacts",
+          imageUrl: product['image_url'],
+        );
+      }).toList();
+    } else {
+      throw Exception("Erreur OpenFoodFacts");
+    }
+  }
+
+  Future<List<Food>> _fetchFromEdamamAPI(String query) async {
+    final apiKey = '24dcad2ef38557ce8c055e032a9e4f16';
+    final appID = 'd4394cd3';
+    final url = 'https://api.edamam.com/api/food-database/v2/parser?ingr=$query&app_id=$appID&app_key=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final foodItems = data['hints'] as List;
+      return foodItems.map((item) {
+        final food = item['food'];
+        return Food(
+          name: food['label'] ?? 'Inconnu',
+          calories: (food['nutrients']?['ENERC_KCAL'] ?? 0).toInt(),
+          carbs: (food['nutrients']?['CHOCDF'] ?? 0).toInt(),
+          fat: (food['nutrients']?['FAT'] ?? 0).toInt(),
+          protein: (food['nutrients']?['PROCNT'] ?? 0).toInt(),
+          sourceApi: "Edamam",
+          imageUrl: food['image'],
+        );
+      }).toList();
+    } else {
+      throw Exception("Erreur Edamam");
     }
   }
 
@@ -102,39 +163,8 @@ class _AddFoodPageState extends State<AddFoodPage> {
       );
 
       widget.onFoodAdded(food);
-      _saveFoodToFirestore(food);
       Navigator.pop(context);
     }
-  }
-
-  Future<void> _saveFoodToFirestore(Food food) async {
-    String? uid = FirebaseAuth.instance.currentUser?.uid;
-    if (uid != null) {
-      try {
-        await FirebaseFirestore.instance
-            .collection('users')
-            .doc(uid)
-            .collection('foods')
-            .add({
-          'name': food.name,
-          'calories': food.calories,
-          'carbs': food.carbs,
-          'fat': food.fat,
-          'protein': food.protein,
-        });
-      } catch (e) {
-        print("Erreur lors de l'enregistrement de l'aliment: $e");
-      }
-    }
-  }
-
-  void _navigateToSearchPage() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => FoodSearchPage(),
-      ),
-    );
   }
 
   @override
@@ -142,96 +172,77 @@ class _AddFoodPageState extends State<AddFoodPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Ajouter un aliment'),
-        backgroundColor: Colors.blueAccent,  // Couleur bleue pour l'app bar
+        backgroundColor: Colors.blueAccent,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Form(
           key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20),
-                _buildTextField(_nameController, "Nom de l'aliment"),
-                _buildTextField(_caloriesController, "Calories", isNumeric: true),
-                _buildTextField(_carbsController, "Glucides", isNumeric: true),
-                _buildTextField(_fatController, "Matières grasses", isNumeric: true),
-                _buildTextField(_proteinController, "Protéines", isNumeric: true),
-                SizedBox(height: 20),
-                ElevatedButton.icon(
-                  onPressed: _scanBarcode,
-                  icon: Icon(Icons.camera_alt, size: 20),
-                  label: Text('Scanner un code-barres'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,  // Couleur bleue pour les boutons
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildTextField(_nameController, "Nom de l'aliment"),
+              _buildTextField(_caloriesController, "Calories", isNumeric: true),
+              _buildTextField(_carbsController, "Glucides", isNumeric: true),
+              _buildTextField(_fatController, "Matières grasses", isNumeric: true),
+              _buildTextField(_proteinController, "Protéines", isNumeric: true),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: _scanBarcode,
+                icon: Icon(Icons.camera_alt, size: 20),
+                label: Text('Scanner un code-barres'),
+              ),
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: "Recherchez un aliment",
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: _searchFoods,
                   ),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton.icon(
-                  onPressed: _navigateToSearchPage,
-                  icon: Icon(Icons.search, size: 20),
-                  label: Text('Rechercher un aliment'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,  // Couleur bleue pour les boutons
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                  ),
+              ),
+              _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(
+                child: ListView.builder(
+                  itemCount: _foodList.length,
+                  itemBuilder: (context, index) {
+                    final food = _foodList[index];
+                    return ListTile(
+                      title: Text(food.name),
+                      subtitle: Text("Calories: ${food.calories}"),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FoodDetailPage(food: food),
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
-                SizedBox(height: 30),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: _submit,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,  // Couleur bleue pour les boutons
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                    ),
-                    child: Text(
-                      'Ajouter',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _buildTextField(TextEditingController controller, String label, {bool isNumeric = false}) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: TextFormField(
-        controller: controller,
-        keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
-        decoration: InputDecoration(
-          labelText: label,
-          labelStyle: TextStyle(color: Colors.blueAccent),  // Texte en bleu pour les labels
-          filled: true,
-          fillColor: Colors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        ),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Veuillez entrer $label';
-          }
-          return null;
-        },
-      ),
+  Widget _buildTextField(TextEditingController controller, String label,
+      {bool isNumeric = false}) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label),
+      keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Veuillez entrer une valeur';
+        }
+        return null;
+      },
     );
   }
 }
