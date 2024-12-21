1b71655a4d0169af2dd84fbfb40b71887e29c688
aymeric <aymeric.job@student.hepl.be>
2024-12-21 19:08:02 +0100

    :zap: Name change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Fitness Pal',
+      title: 'NutriTrack',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
